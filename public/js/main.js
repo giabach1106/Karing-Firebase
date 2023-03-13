a9e829e35c5b4d09b029a2e0a9ec0f4b5c308cfc
Zor1k <giabachand@gmail.com>
2023-03-13 20:25:32 +0700

    update

--- public/js/main.js
+++ public/js/main.js
@@ -54,7 +54,7 @@ const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
 const db = getDatabase();
-const aricleRef = ref(db, 'dt');
+const aricleRef = ref(db, 'db/' +'pt');
 
 
 let id = getUrlParameter("id")
@@ -62,29 +62,30 @@ let id = getUrlParameter("id")
 // console.log(aricleRef2);
 
 
-let x = ['dt', 'pt'];
-for (let i=0;i<2;i++){
-    let id = x[i];
-    const aricleRef2 = ref(db, 'db/'+ id);
-    onValue(aricleRef2, (snapshot) => {
-        const articles = snapshot.val();
-        for (var article in articles) {
-        $("#article").append(articles[article].name + "<br>" + articles[article].dob)
-        if(articles[article].image){
-            $("#article").append("<br><img src = '"+articles[article].image+"'>")
-        }
-        $("#article").append("<a style ='color:white;text-decoration:none' href='dt.html?id="+article+"'>"+"<div class = 'card-body'>"+"<div class = 'btn btn-primary'>"+ "View Profile" +"<a>")
-        $("#article").append("<hr>")
-        }
-        $("#loading").hide()
-        });
-}
+// let x = ['dt', 'pt'];
+// for (let i=0;i<2;i++){
+//     let id = x[i];
+//     const aricleRef2 = ref(db, 'db/'+ id);
+//     onValue(aricleRef2, (snapshot) => {
+//         const articles = snapshot.val();
+//         for (var article in articles) {
+//         $("#article").append(articles[article].name + "<br>" + articles[article].dob)
+//         if(articles[article].image){
+//             $("#article").append("<br><img src = '"+articles[article].image+"'>")
+//         }
+//         $("#article").append("<a style ='color:white;text-decoration:none' href='dt.html?id="+article+"'>"+"<div class = 'card-body'>"+"<div class = 'btn btn-primary'>"+ "View Profile" +"<a>")
+//         $("#article").append("<hr>")
+//         }
+//         $("#loading").hide()
+//         });
+// }
+
 // onValue(aricleRef, (snapshot) => {
 // const articles = snapshot.val();
 // for (var article in articles) {
-// $("#article").append(articles[article].name + "<br>" + articles[article].dob)
-// if(articles[article].ava){
-//     $("#article").append("<br><img src = '"+articles[article].ava+"'>")
+// $("#article").append(articles[article].name + "<br>" + articles[article].sex)
+// if(articles[article].image){
+//     $("#article").append("<br><img src = '"+articles[article].image+"'>")
 // }
 // $("#article").append("<hr>")
 // }
@@ -92,6 +93,45 @@ for (let i=0;i<2;i++){
 // });
 
 
+// onValue(aricleRef, (snapshot) => {
+//   const articles = snapshot.val();
+//   for (var article in articles) {
+//    $("#article").append("<a href='article.html?id="+article+"'>" + articles[article].name + "<br>" + articles[article].age + "<a>")
+//    if(articles[article].sex == "Nữ")
+//         $("#pt-name").append("<a href='pt.html?id="+article+"'>"+"Bà " + articles[article].name + "<br>")
+//    if(articles[article].sex == "Nam")
+//         $("#pt-name").append("<a href='pt.html?id="+article+"'>"+"Ông " + articles[article].name+ "<br>")
+// //    $("#pt-name").append("<a href='pt.html?id="+article+"'>" +articles[article].name + "<br>")
+
+//    $("#pt-place").append(articles[article].place + "<br>")
+//    $("#pt-age").append(articles[article].age + "<br>")
+//    $("#pt-sex").append(articles[article].sex + "<br>")
+//    $("#article").append("<hr>")
+//     if(articles[article].image){
+//         $("#pt-img").append("<img src = '"+articles[article].image+"'>")
+//         }
+//     $("#pt-img").append("<hr>")
+//   }
+
+
+//   $("#loading").hide()
+// });
+
+
+onValue(aricleRef, (snapshot) => {
+    const articles = snapshot.val();
+    for (var article in articles) {
+        if(articles[article].sex == "Nam")
+            articles[article].name = "Ông "+articles[article].name;
+        if(articles[article].sex == "Nữ")
+            articles[article].name = "Bà "+articles[article].name;
+        
+        $("#article").append("<div class='patient-info'>"+"<img src = '"+articles[article].image+"'>"+"<div class='info'>" + "<a href='pt.html?id="+article+"'>"+"<h3>" + articles[article].name + "</h3></a>"+"<div class='ttll'> <div class='locate'> <img src='/public/image/locate.svg' alt=''>" + "<h5>"+articles[article].place +"</h5>" + "</div>" + "<div class='line3'> <div class='age'> <h5 class='dark'>Tuổi:</h5>"+ "<h5>"+ articles[article].age +"</h5>"+"</div><div class='gender'><h5 class='dark'>Giới tính:</h5>"+ "<h5>"+articles[article].sex +"</h5></div></div></div></div>")
+    }
+  
+  
+    $("#loading").hide()
+  });
 
 // onValue(aricleRef2, (snapshot) => {
 // const article = snapshot.val();
